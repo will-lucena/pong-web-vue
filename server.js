29aa92258c94ac4bdca920f135c7e4de0002fec4
Will Lucena <will.lucen4@gmail.com>
2025-02-25 22:52:20 -0300

    add express server to run on heroku

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import serveStatic from 'serve-static'
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const app = express()
+
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+	res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log(`app is listening on port: ${port}`)
